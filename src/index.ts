595cd848e90833064bcc0a048f0cd39f5787d973
nickreynolds <nicholas.s.reynolds@gmail.com>
2022-12-16 22:50:29 -0500

    create ens-did-provider

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,4 @@
  */
 const schema = require('../plugin.schema.json')
 export { schema }
-export { MyAgentPlugin } from './agent/my-plugin'
-export { MyKeyManagementSystem } from './key-manager/my-key-management-system'
-export { MyKeyStore } from './key-manager/my-key-store'
-export { SecretBox } from './key-manager/my-secret-box'
-export { MyIdentifierProvider } from './did-manager/my-identifier-provider'
-export { MyDIDStore } from './did-manager/my-did-store'
-export * from './types/IMyAgentPlugin'
+export { EnsDIDProvider } from './did-manager/ens-did-provider'
